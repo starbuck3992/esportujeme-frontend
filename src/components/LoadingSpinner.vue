edc42edf3572e3ecc65327686be3c317300c2cbf
svardys <75584296+svardys@users.noreply.github.com>
2022-09-20 11:12:59 +0200

    Menu - basics

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,6 +1,7 @@
 <template>
   <div v-show="loading" class="flex justify-center items-center space-x-2 fixed inset-0 z-50 w-[95%] sm:max-w-7xl mx-auto">
-    <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full animate-bounce text-blue-900" role="status"></div>
+    <div class="spinner-grow
+    inline-block w-8 h-8 bg-current rounded-full animate-bounce text-blue-900" role="status"></div>
     <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full animation-delay-100 animate-bounce text-blue-800" role="status"></div>
     <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full animation-delay-200 animate-bounce text-blue-700" role="status"></div>
     <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full animation-delay-300 animate-bounce text-blue-600" role="status"></div>
