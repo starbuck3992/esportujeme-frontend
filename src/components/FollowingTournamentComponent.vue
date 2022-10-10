101eefb3bc13b3f83df69cf9198bca1375b7fe66
svardys <75584296+svardys@users.noreply.github.com>
2022-10-10 18:18:55 +0200

    Build development for Number Parser and Animation testing

--- src/components/FollowingTournamentComponent.vue
+++ src/components/FollowingTournamentComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="relative w-full h-full max-w-[100rem] mx-auto px-5">
     <div class="bg-transparent pt-16 lg:py-24">
-      <div class="bg-gray-800 pb-16 lg:relative lg:z-10 lg:pb-0 rounded-lg">
+      <div class="bg-[#1f1f28] border-gray-800 border pb-16 lg:relative lg:z-10 lg:pb-0 rounded-lg">
         <div class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
           <div class="relative lg:-my-8">
-            <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-gray-800 lg:hidden rounded-lg" />
+            <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-gray-900 lg:hidden rounded-lg" />
             <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:h-full lg:p-0">
               <div class="aspect-w-10 aspect-h-6 overflow-hidden rounded-xl shadow-xl sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
                 <img class="object-cover lg:h-full lg:w-full" src="@/assets/images/fifa.jpg" alt="" />
@@ -35,7 +35,7 @@
     <div class="my-5">
     <h3 class="font-medium text-5xl text-white font-oswald mb-5">Odměny za umístění:</h3>
     <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-      <div v-for="item in rewards" :key="item.title" class="overflow-hidden rounded-lg bg-gray-800 px-4 py-5 shadow sm:p-5">
+      <div v-for="item in rewards" :key="item.title" class="overflow-hidden rounded-lg bg-[#1f1f28] px-4 py-5 shadow sm:p-5">
         <dt class="text-3xl font-bold text-[#ff8503] text-center mb-5">
           <svg :class="['inline-block h-10 w-10 my-auto align-middle mr-2',item.color]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0" />
